757e1f7b5f74980d1ce3a67fcb1055607650b1a5
Matin Nazamy <matin.nazamy50@qmail.cuny.edu>
2022-02-19 16:31:33 -0500

    library class

---
+++ Library.py
@@ -0,0 +1,16 @@
+class Library:
+
+    def __init__(self, tempLib):
+            self.id = int(tempLib["id"])
+            self.numBooks = int(tempLib["NumBooksInThisLibrary"] )
+            self.signUpProcess = int (tempLib["SignupProcess"] )
+            self.booksPerDay= int( tempLib["BooksPerDay"] )
+            self.books = tempLib["BooksArray"] 
+            self.score = int(tempLib["Score"])
+        
+    def print(self):
+        bookString = ""
+        for i in self.books:
+            bookString += str(i) + " "
+        
+        print("---------------------\nLibrary ID: " + str(self.id) + "\nnumBooks " + str(self.numBooks) + "\nSign Up Length " + str(self.signUpProcess) + "\n Books Shipped Per Day" + str(self.booksPerDay ) + "\n Books : " + bookString + " \n Score: " + str(self.score))
